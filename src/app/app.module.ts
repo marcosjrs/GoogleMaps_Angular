85cb195f1829c02b156b25f4c79e43ac056cd20a
marcosjrs <marcosjrs80@gmail.com>
2018-11-06 12:44:45 +0100

    Pruebas de diferentes Componentes de angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+//Modulos de Angular Material
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,8 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
-
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
