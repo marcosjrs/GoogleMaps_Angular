37db32e27f762970a7e02e17879257d770f0714d
marcosjrs <marcosjrs80@gmail.com>
2018-11-06 15:15:08 +0100

    Pruebas: Añadiendo marcadores de forma dinámica al pulsar en el mapa

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Marcador } from 'src/app/models/marcador';
 
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.component.html'
 })
 export class MapaComponent implements OnInit {
-  lat: number = 51.678418;
-  lng: number = 7.809007;
 
-  constructor() { }
+  marcadores:Marcador[];
+  latMapa: number = 43.38596317684374;
+  lngMapa: number = -8.4065151;
+
+  constructor() {    
+    //Para pruebas
+    this.marcadores = [new Marcador(43.38596317684374, -8.4065151, "A Coruña","Torre de Hércules" )]
+  }
 
   ngOnInit() {
+  }
+
+  addMarcador(evento){
+    this.marcadores.push( new Marcador(evento.coords.lat, evento.coords.lng) );
   }
 
 }
---
+++ src/app/models/marcador.ts
@@ -0,0 +1,5 @@
+export class Marcador {
+    constructor(public lat:number, public lng:number, public titulo:string="", public desc:string=""){
+
+    }
+}
